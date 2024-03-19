c8feafe5c7e65c8d424de037a4d04229d1dd97be
Otto-Cristofer Vanasaun <ottovanasaun@gmail.com>
2024-03-19 18:28:05 +0200

    mandariini alginfo

---
+++ src/Mandariin.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+
+public class Mandariin {
+    private int mängijaAsukoht;
+    private int gravitatsioon;
+    private int mängijaKõrgus;
+    private int mängijaLaius;
+
+    public int getMängijaAsukoht() {
+        return mängijaAsukoht;
+    }
+
+    public int getGravitatsioon() {
+        return gravitatsioon;
+    }
+
+    public int getMängijaKõrgus() {
+        return mängijaKõrgus;
+    }
+
+    public int getMängijaLaius() {
+        return mängijaLaius;
+    }
+
+    public void setGravitatsioon(int gravitatsioon) {
+        this.gravitatsioon = gravitatsioon;
+    }
+
+    public void setMängijaAsukoht(int mängijaAsukoht) {
+        this.mängijaAsukoht = mängijaAsukoht;
+    }
+
+    public Mandariin(int mängijaAsukoht, int gravitatsioon, int mängijaKõrgus, int mängijaLaius) {
+        this.mängijaAsukoht = mängijaAsukoht;
+        this.gravitatsioon = gravitatsioon;
+        this.mängijaKõrgus = mängijaKõrgus;
+        this.mängijaLaius = mängijaLaius;
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.ORANGE);
+        g.fillRect(150, mängijaAsukoht, mängijaKõrgus, mängijaLaius);
+    }
+
+}
